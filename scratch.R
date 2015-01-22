24a892a6d39d3a3470156faa0af547746ec6cee5
Aaron Dossett <aaron.dossett@target.com>
2015-01-22 15:34:19 -0600

    Add write-up shell

--- scratch.R
+++ scratch.R
@@ -16,7 +16,7 @@ raw <- raw[, ! grepl("timestamp", names(raw))]
 #source for this nice code: http://stackoverflow.com/a/11330265
 raw <- raw[, ! sapply(raw, function(x)all(is.na(x)))]
 
-#Analysis shows that many, many values are missing.
+#Analysis shows that many, many columns are almost completely missing.
 #Throw out any column that is > 90% null.  Adapted from above
 nullCounts <- sapply(raw, function(x)sum(is.na(x)))
 raw <- raw[, nullCounts < (19622 * .9)]
@@ -24,13 +24,19 @@ raw <- raw[, nullCounts < (19622 * .9)]
 #Git rid of any columns with near zero variance (in practice, one column -- "new window")
 raw <- raw[, ! nearZeroVar(raw, saveMetrics = T)[,4]]
 
-# Split into training and testing.  Never to look at training until the very end
+# Split into training, testing, and validation.  Never to look at testing until the very end!
+# 70% training, 15% validation, and 15% testing
 set.seed(1976)
-inTrain <- createDataPartition(y=raw$classe, p=.8, list=FALSE)
+inTrain <- createDataPartition(y=raw$classe, p=.7, list=FALSE)
 training <- raw[inTrain,]
-testing <- raw[-inTrain,]
+testAndVal <- raw[-inTrain,]
+inVal <- createDataPartition(y=testAndVal$classe, p=.15, list=FALSE)
+validation <- testAndVal[inVal,]
+testing <- testAndVal[-inVal,]
 
 
-#PLAY -- flipped train and test for effeciency
-modelFit <- train(testing$classe ~ ., method="rf", data=testing)
-confusionMatrix(training$classe, predict(modelFit, training))
+#Look at three models -- treebag, random forest, and nb
+treeBagFit <- train(training$classe ~ ., method="treebag", data=training)
+randomForestFit <- train(training$classe ~ ., method="treebag", data=training)
+naiveBayesFit <- train(training$classe ~ ., method="treebag", data=training)
+#confusionMatrix(testing$classe, predict(modelFit, testing))
